866ebcfc29915264cd92734a467a455b7b1e35f9
Zachary Claret-Scott <zachary.claretscott@probablyrational.com>
2022-12-10 01:16:22 +0000

    Completed step 2 finding badges
    Signed-off-by: Zachary Claret-Scott <zachary.claretscott@probablyrational.com>

--- app/Commands/AOC2022/AOC2022Three.php
+++ app/Commands/AOC2022/AOC2022Three.php
@@ -81,6 +81,37 @@ To help prioritize item rearrangement, every item type can be converted to a pri
 In the above example, the priority of the item type that appears in both compartments of each rucksack is 16 (p), 38 (L), 42 (P), 22 (v), 20 (t), and 19 (s); the sum of these is 157.
 
 Find the item type that appears in both compartments of each rucksack. What is the sum of the priorities of those item types?
+
+--- Part Two ---
+As you finish identifying the misplaced items, the Elves come to you with another issue.
+
+For safety, the Elves are divided into groups of three. Every Elf carries a badge that identifies their group. For efficiency, within each group of three Elves, the badge is the only item type carried by all three Elves. That is, if a group's badge is item type B, then all three Elves will have item type B somewhere in their rucksack, and at most two of the Elves will be carrying any other item type.
+
+The problem is that someone forgot to put this year's updated authenticity sticker on the badges. All of the badges need to be pulled out of the rucksacks so the new authenticity stickers can be attached.
+
+Additionally, nobody wrote down which item type corresponds to each group's badges. The only way to tell which item type is the right one is by finding the one item type that is common between all three Elves in each group.
+
+Every set of three lines in your list corresponds to a single group, but each group can have a different badge item type. So, in the above example, the first group's rucksacks are the first three lines:
+
+----
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+----
+
+And the second group's rucksacks are the next three lines:
+
+----
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+----
+
+In the first group, the only item type that appears in all three rucksacks is lowercase r; this must be their badges. In the second group, their badge item type must be Z.
+
+Priorities for these items must still be found to organize the sticker attachment efforts: here, they are 18 (r) for the first group and 52 (Z) for the second group. The sum of these is 70.
+
+Find the item type that corresponds to the badges of each three-Elf group. What is the sum of the priorities of those item types?
 EOL);
                 break;
             case 1:
@@ -98,11 +129,30 @@ EOL);
 
                 $priorityTotal = number_format($priorityTotal);
 
-                $this->alert("The total priority is {$priorityTotal}");
+                $this->alert("The total priority is $priorityTotal");
                 break;
             case 2:
                 $this->info('Running step 2');
                 $this->loadData();
+
+                $chunks = $this->rucksacks->chunk(3);
+
+                $priorityTotal = 0;
+                foreach($chunks as $chunk) {
+                    $elfOneRucksack = $chunk->values()[0]['compartment_1'] . $chunk->values()[0]['compartment_2'];
+                    $elfTwoRucksack = $chunk->values()[1]['compartment_1'] . $chunk->values()[1]['compartment_2'];
+                    $elfThreeRucksack = $chunk->values()[2]['compartment_1'] . $chunk->values()[2]['compartment_2'];
+
+                    $itemsOnAllElves = implode(array_unique(array_intersect(str_split($elfOneRucksack), str_split($elfTwoRucksack), str_split($elfThreeRucksack))));
+
+                    // I had hoped I could change 96 to 64 depending on capital or lowercase, but that didn't work
+                    $number = (ord(strtolower($itemsOnAllElves)) - 96) + ($itemsOnAllElves == strtoupper($itemsOnAllElves) ? 26 : 0);
+                    if($number > 0) $priorityTotal = $priorityTotal + $number;
+                }
+
+                $priorityTotal = number_format($priorityTotal);
+
+                $this->alert("The total priority is $priorityTotal");
                 break;
         }
 
@@ -117,6 +167,8 @@ EOL);
 
         $this->rucksacks = Collect([]);
         foreach(explode("\n", $data) as $line) {
+            if($line == '') continue;
+
             $first_half = substr($line,0, strlen($line)/2);
             $second_half = substr($line, strlen($line)/2);
 
