1395b0ccb5b093fea1d028c993b84b7d682b40b4
Zachary Claret-Scott <zachary.claretscott@probablyrational.com>
2022-12-10 00:09:42 +0000

    Cleaned up menu
    Signed-off-by: Zachary Claret-Scott <zachary.claretscott@probablyrational.com>

---
+++ app/Enums/AOC2022GameObjects.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Enums;
+
+enum AOC2022GameObjects {
+    case ROCK;
+    case PAPER;
+    case SCISSORS;
+
+    public function beats(): self
+    {
+        return match($this) {
+            self::ROCK => self::SCISSORS,
+            self::PAPER => self::ROCK,
+            self::SCISSORS => self::PAPER,
+        };
+    }
+
+    public function losesTo(): self
+    {
+        return match($this) {
+            self::ROCK => self::PAPER,
+            self::PAPER => self::SCISSORS,
+            self::SCISSORS => self::ROCK,
+        };
+    }
+
+    public function score(): int
+    {
+        return match($this) {
+            self::ROCK => 1,
+            self::PAPER => 2,
+            self::SCISSORS => 3,
+        };
+    }
+}
