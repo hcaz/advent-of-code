3a8e05c2231adefdac416f76383844829af6f102
Zachary Claret-Scott <zachary.claretscott@probablyrational.com>
2022-12-10 12:16:04 +0000

    Updated code for 14 chars too
    Signed-off-by: Zachary Claret-Scott <zachary.claretscott@probablyrational.com>

--- app/Commands/AOC2022/AOC2022.php
+++ app/Commands/AOC2022/AOC2022.php
@@ -33,7 +33,7 @@ class AOC2022 extends Command
             'Day 3: Rucksack Reorganization ★★',
             'Day 4: Camp Cleanup ★★',
             'Day 5: Supply Stacks ★★',
-            'Day 6: Tuning Trouble',
+            'Day 6: Tuning Trouble ★★',
             'Day 7: No Space Left On Device',
             'Day 8: Treetop Tree House',
             'Day 9: Rope Bridge',
