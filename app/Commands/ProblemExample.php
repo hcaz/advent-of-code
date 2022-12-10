f27d762d5c4f2ff3ed4bd911743bd5226142e351
Zachary Claret-Scott <zachary.claretscott@probablyrational.com>
2022-12-10 10:38:53 +0000

    Create example problem file

---
+++ app/Commands/ProblemExample.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Commands;
+
+use Illuminate\Support\Facades\Storage;
+use LaravelZero\Framework\Commands\Command;
+
+class ProblemExample extends Command
+{
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'browse/year/problem';
+
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'Display solution for problem # :: YEAR';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $option = $this->menu('Day #: Title', [
+            'View problem',
+            'Run step 1',
+            'Run step 2',
+        ])->setExitButtonText("Back")->open();
+
+        if(is_null($option)) {
+            return;
+        }
+
+        switch($option) {
+            case 0:
+                $this->alert('https://adventofcode.com/YEAR/day/PROBLEM');
+                $this->info(<<<EOL
+
+EOL);
+                break;
+            case 1:
+                $this->info('Running step 1');
+                $this->loadData();
+                break;
+            case 2:
+                $this->info('Running step 2');
+                $this->loadData();
+                break;
+        }
+
+        $this->ask('Press any key to continue');
+        $this->handle();
+    }
+
+    private function loadData() {
+        $this->info("Running solution for problem # :: YEAR");
+        $this->info("Loading in 2022_four_input.txt");
+        $data = Storage::get('2022/four_input.txt');
+
+
+    }
+}
